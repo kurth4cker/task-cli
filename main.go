3c920f6f76d8b1c0bf8078379f35b6dd572ddbf4
kurth4cker <kurth4cker@gmail.com>
2025-01-14 11:11:24 +0300

    main.go: add task struct and json encoding support

--- main.go
+++ main.go
@@ -6,5 +6,44 @@
 // Created for a project idea taken from roadmap.sh
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+)
+
+type task struct {
+	Id          uint   `json:"id"`
+	Description string `json:"description"`
+
+	// One of "done", "todo", "in-progress"
+	Status string `json:"status"`
+
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
 func main() {
+	dataPath := "tasks.json"
+	data, err := os.ReadFile(dataPath)
+	if err != nil && os.IsNotExist(err) {
+		log.Fatalln(err)
+	}
+
+	var tasks []task
+	json.Unmarshal(data, &tasks)
+
+	// do whatever you want with tasks
+
+	data, err = json.Marshal(tasks)
+	var buffer bytes.Buffer
+	json.Indent(&buffer, data, "", "    ")
+	buffer.WriteRune('\n')
+	data = buffer.Bytes()
+	err = os.WriteFile(dataPath, data, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
