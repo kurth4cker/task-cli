e79f5f2a17c105c286efe61408837e643b927c02
kurth4cker <kurth4cker@gmail.com>
2025-01-16 14:24:32 +0300

    main.go: findNextId(): add tests

---
+++ task_test.go
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package main
+
+import "testing"
+
+func TestFindNextId(t *testing.T) {
+	assertGotWant := func(t testing.TB, got, want uint) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertGotNotWant := func(t testing.TB, got, notWant uint) {
+		t.Helper()
+		if got == notWant {
+			t.Errorf("do not want %q but got it", got)
+		}
+	}
+
+	t.Run("should start from zero", func(t *testing.T) {
+		got := findNextId([]task{})
+		var want uint = 0
+
+		assertGotWant(t, got, want)
+	})
+
+	t.Run("should not duplicate", func(t *testing.T) {
+		tasks := []task{
+			{Id: 0},
+			{Id: 1},
+			{Id: 2},
+			{Id: 3},
+			{Id: 4},
+			{Id: 5},
+		}
+
+		got := findNextId(tasks)
+		for _, task := range tasks {
+			assertGotNotWant(t, got, task.Id)
+		}
+	})
+}
