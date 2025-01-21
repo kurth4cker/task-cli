7dd85e985739e5c785b48863b51b1bffb8d91086
kurth4cker <kurth4cker@gmail.com>
2025-01-21 15:08:59 +0300

    task.Task: add UnmarshalJSON test

---
+++ internal/task/task_test.go
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package task
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTaskUnmarshalJSON(t *testing.T) {
+	t.Run("should work with json.Unmarshal", func(t *testing.T) {
+		data := []byte(`
+{
+    "id": 1,
+    "description": "task 1",
+    "status": "todo"
+}
+`)
+		var got Task
+		if err := json.Unmarshal(data, &got); err != nil {
+			t.Fatalf("unmarshal failed: %e", err)
+		}
+		want := Task{
+			Id:          1,
+			Description: "task 1",
+			Status:      "todo",
+		}
+
+		if got != want {
+			t.Errorf("got %v, want %v, given %q",
+				got,
+				want,
+				string(data))
+		}
+	})
+}
