e1f4269b6ac50f9bbb34de2cb3d03f9a45d790da
kurth4cker <kurth4cker@gmail.com>
2025-02-03 19:52:00 +0300

    main: add dependency tests

---
+++ task_test.go
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package main
+
+import (
+	"slices"
+	"testing"
+
+	"codeberg.org/kurth4cker/task-cli-go/internal/task"
+)
+
+func TestSetAddDescription(t *testing.T) {
+	want := []string{
+		"task 1",
+		"task 2",
+	}
+
+	var set task.Set
+	for _, description := range want {
+		set.AddDescription(description)
+	}
+
+	var got []string
+	for task := range set.All() {
+		got = append(got, task.Description)
+	}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestSetAll(t *testing.T) {
+	t.Run("empty task set", func(t *testing.T) {
+		want := 0
+
+		var set task.Set
+		got := len(slices.Collect(set.All()))
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
