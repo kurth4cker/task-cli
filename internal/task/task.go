222b34874cfce8e7d3a8592f6ef9aa428d99ec8c
kurth4cker <kurth4cker@gmail.com>
2025-02-03 20:08:27 +0300

    task: add package documentation

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
 
+// The package task implements task manipulating type and methods.
+//
+// You can use it for adding, deleting and tracking a task. Also you can track
+// for a task's status by a task id.
+//
+// Generally you do not need for direct use about Task. You should use Set.
+// Which operates all related Task's. You can search and update any Task with
+// id.
 package task
 
 import "fmt"
