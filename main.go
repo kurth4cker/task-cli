9de557b2d63ca3861e74842587e7d690ca371c87
kurth4cker <kurth4cker@gmail.com>
2025-02-28 23:57:35 +0300

    main: add TODO comments

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
 
-// Task management CLI application
-//
-// Created for a project idea taken from roadmap.sh
+/*
+PLAN:
+- [TODO] Check for tasks.json existence. If it is not exist, create it.
+- [TODO] tasks.json would not be an json array, it would be series of json objects
+- [TODO] we need a Task structure for json
+- [TODO] we need an Set structure
+	- [TODO] Set should parse file which is a series of json Task objects
+	- [TODO] Set should lookup for objects with given Id
+	- [TODO] Methods:
+		- [TODO] Add(id)
+		- [TODO] Del(id)
+		- [TODO] Update(id)
+*/
+
+/*
+Task management CLI application
+
+Created for a project idea taken from roadmap.sh
+*/
 package main
 
 import "codeberg.org/kurth4cker/go-sample"
 
+/*
+TODO: write sub commands here
+*/
 func main() {
 	sample.Helloln("world")
 }
