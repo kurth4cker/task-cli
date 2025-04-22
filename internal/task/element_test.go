128b81ddb09baa1326760a1e94f50368a3d5eb56
kurth4cker <kurth4cker@gmail.com>
2025-04-22 16:28:37 +0300

    task: add element JSON tests

---
+++ internal/task/element_test.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package task_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/kurth4cker/task-cli/internal/task"
+)
+
+func TestElement_JSON(t *testing.T) {
+	element := task.Element{
+		Id:          0,
+		Description: "Task 1",
+	}
+
+	var got task.Element
+	want := element
+	{
+		tmp, err := json.Marshal(&element)
+		if err != nil {
+			t.Fatalf("marshal, unexpected error: %s", err)
+		}
+		err = json.Unmarshal(tmp, &got)
+		if err != nil {
+			t.Fatalf("unmarshal, unexpected error: %s", err)
+		}
+	}
+
+	if got != want {
+		t.Errorf("got element %v, want %v", got, want)
+	}
+}
