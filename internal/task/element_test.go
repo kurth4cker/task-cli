40aca087375391cf467e606f66e68357c21ffa82
kurth4cker <kurth4cker@gmail.com>
2025-05-04 11:48:14 +0300

    task: add status

--- internal/task/element_test.go
+++ internal/task/element_test.go
@@ -33,3 +33,24 @@ func TestElement_JSON(t *testing.T) {
 		t.Errorf("got element %v, want %v", got, want)
 	}
 }
+
+func TestStatus_String(t *testing.T) {
+	cases := []struct{
+		given task.Status
+		want string
+	}{
+		{given: task.Done, want: "done"},
+		{given: task.InProgress, want: "in-progress"},
+		{given: task.Todo, want: "todo"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.want, func(t *testing.T) {
+			want := c.want
+			got := string(c.given)
+			if got != want {
+				t.Errorf("got %v, want %v, given %v", got, want, c.given)
+			}
+		})
+	}
+}
